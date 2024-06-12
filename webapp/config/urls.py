9172be6f779ce8762ac826cc6759e735418a2d48
jslee <dlwhdtjd098@gmail.com>
2024-06-12 20:08:35 +0900

    feat: media url 설정

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
-
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.http import JsonResponse
+from django.urls import path, include
 
 
 def health_check(request):
@@ -29,7 +29,7 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
     path("health/", health_check, name="health_check"),
-    path("", include("shop.urls")),
+    path("", include("shop.urls", namespace="shop")),
 ]
 
 if settings.DEBUG:
@@ -37,3 +37,6 @@ if settings.DEBUG:
         urlpatterns += [
             path("__debug__/", include("debug_toolbar.urls")),
         ]
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
