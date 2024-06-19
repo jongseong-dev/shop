1b73ea57af3da12aab262a10ab91ef29e02b6603
jslee <dlwhdtjd098@gmail.com>
2024-06-19 22:49:40 +0900

    feat(orders.views): 주문 할 때 cart에 상품이 있어야 등록되도록 flag 추가

--- webapp/orders/views.py
+++ webapp/orders/views.py
@@ -9,7 +9,7 @@ def order_create(request):
     cart = Cart(request)
     if request.method == "POST":
         form = OrderCreateForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and cart.__len__() > 0:
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(
