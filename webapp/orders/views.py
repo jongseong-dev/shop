132645f956e1906c89fe5243e19966e47f919060
jslee <dlwhdtjd098@gmail.com>
2024-06-20 19:07:34 +0900

    feat(orders): 이메일 비동기로 보내는 task 등록 및 사용

--- webapp/orders/views.py
+++ webapp/orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
 from orders.models import OrderItem
+from orders.tasks import order_created
 
 
 def order_create(request):
@@ -19,6 +20,7 @@ def order_create(request):
                     quantity=item["quantity"],
                 )
             cart.clear()
+            order_created.delay(order.id)
             return render(
                 request,
                 "orders/order/created.html",
