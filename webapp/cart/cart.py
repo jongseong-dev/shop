96f705feaa2f3a87d22782e32dda9877c860c4e0
jslee <dlwhdtjd098@gmail.com>
2024-06-26 22:48:44 +0900

    feat(coupon): cart.py에 coupon 적용하는 로직 추가 및 coupon 적용 views 추가

--- webapp/cart/cart.py
+++ webapp/cart/cart.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 
 from django.conf import settings
 
+from coupons.models import Coupon
 from shop.models import Product
 
 
@@ -13,6 +14,24 @@ class Cart:
             # 세션에 카트가 없으면 빈 딕셔너리를 저장
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # 현재 적용된 쿠폰 저장
+        self.coupon_id = self.session.get("coupon_id")
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (
+                self.coupon.discount / Decimal(100)
+            ) * self.get_total_price()
+        return Decimal(0)
 
     def add(
         self,
@@ -86,6 +105,9 @@ class Cart:
             Decimal(item["price"]) * item["quantity"]
             for item in self.cart.values()
         )
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
 
     def clear(self):
         # 카트 비우기
--- webapp/coupons/views.py
+++ webapp/coupons/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from coupons.forms import CouponApplyForm
+from coupons.models import Coupon
+
+
+@require_POST
+def coupon_apply(request):
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data["code"]
+        try:
+            coupon = Coupon.objects.get(
+                code__iexact=code,  # 대소문자를 구분하지 않고 정확히 일치하는 필드값 조회
+                valid_from__lte=now,
+                valid_to__gte=now,
+                active=True,
+            )
+            request.session["coupon_id"] = coupon.id
+        except Coupon.DoesNotExist:
+            request.session["coupon_id"] = None
+    return redirect("cart:cart_detail")
