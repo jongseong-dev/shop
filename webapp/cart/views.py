961e71fad7fd0877d068fe991eb71add529867b5
jslee <dlwhdtjd098@gmail.com>
2024-06-30 23:38:56 +0900

    feat: 카트에 추천 상품 뜨도록 기능 추가

--- webapp/cart/views.py
+++ webapp/cart/views.py
@@ -6,6 +6,7 @@ from cart.cart import Cart
 from cart.forms import CartAddProductForm
 from coupons.forms import CouponApplyForm
 from shop.models import Product
+from shop.recommender import Recommender
 
 
 @require_POST
@@ -42,8 +43,22 @@ def cart_detail(request: HttpRequest):
             }
         )
     coupon_apply_form = CouponApplyForm()
+
+    r = Recommender()
+    cart_products = [item["product"] for item in cart]
+    if cart_products:
+        recommended_products = r.suggest_products_for(
+            cart_products, max_results=4
+        )
+    else:
+        recommended_products = []
+
     return render(
         request,
         "cart/detail.html",
-        {"cart": cart, "coupon_apply_form": coupon_apply_form},
+        {
+            "cart": cart,
+            "coupon_apply_form": coupon_apply_form,
+            "recommended_products": recommended_products,
+        },
     )
