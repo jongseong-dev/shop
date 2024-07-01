3ccacffbeed72f6b72dd4b3f20b34192e9e2a776
jslee <dlwhdtjd098@gmail.com>
2024-07-02 01:17:37 +0900

    refactory(recommender): recommender의 redis storage를 주입받는 형태로 수정
    1. 테스트 코드를 짤 때 fake instance를 주입하기 위해
    2. 주입 받을 때 싱글톤의 storage를 기본값으로 받아 불필요하게 메모리 낭비를 막음

--- webapp/shop/recommender.py
+++ webapp/shop/recommender.py
@@ -3,17 +3,35 @@ from django.conf import settings
 
 from shop.models import Product
 
-# Reids 연결
 
-r = redis.Redis(
-    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
-)
+# Reids 연결
+class RedisSingleton:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = redis.Redis(
+                host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB,
+            )
+        return cls._instance
+
+
+cache_instance = RedisSingleton.get_instance()
 
 
 class Recommender:
     """
     제품 구매 내역을 저장하고 특정 제품에 맞는 추천 제품을 조회할 수 있는 클래스
     """
+
+    def __init__(self, cache_storage=None):
+        if cache_storage is not None:
+            self._cache_storage = cache_storage
+        else:
+            self._cache_storage = cache_instance
 
     def get_product_key(self, id):
         return f"product:{id}:purchased_with"
@@ -30,13 +48,15 @@ class Recommender:
                 # 각 제품과 함께 구매한 다른 제품 획득
                 if product_id != with_id:
                     # 함께 구매한 제품의 점수 증가
-                    r.zincrby(self.get_product_key(product_id), 1, with_id)
+                    self._cache_storage.zincrby(
+                        self.get_product_key(product_id), 1, with_id
+                    )
 
     def suggest_products_for(self, products, max_results=6):
         products_ids = [p.id for p in products]
         if len(products) == 1:
             # 단 1개의 제품
-            suggestions = r.zrange(
+            suggestions = self._cache_storage.zrange(
                 self.get_product_key(products_ids[0]), 0, -1, desc=True
             )[:max_results]
         else:
@@ -46,17 +66,19 @@ class Recommender:
             # 주어진 각 제품들에 함께 구매한 제품들의 점수 합산
             # 결과가 정렬된 세트를 임시 키에 저장
             keys = [self.get_product_key(id) for id in products_ids]
-            r.zunionstore(
+            self._cache_storage.zunionstore(
                 tmp_key, keys
             )  # zuninstaore: 주어진 키들에 해당하는 정렬된 집합들에 대해
             # 유니온을 수행하면서 동일한 요소들의 점수를 집계한다.
 
             # 처음에 주어진 제품들의 ID를 추천 목록에서 제거, 동일한 제품이 추천목록에 뜨는 것을 방지
-            r.zrem(tmp_key, *products_ids)
+            self._cache_storage.zrem(tmp_key, *products_ids)
             # 점수를 기준으로 역정렬해서 제품 id 목록을 가져옴
-            suggestions = r.zrange(tmp_key, 0, -1, desc=True)[:max_results]
+            suggestions = self._cache_storage.zrange(
+                tmp_key, 0, -1, desc=True
+            )[:max_results]
             # 임시 키 제거
-            r.delete(tmp_key)
+            self._cache_storage.delete(tmp_key)
 
         suggested_products_ids = [int(id) for id in suggestions]
         # 추천 제품 정보 조회 및 순서대로 정렬해 표시
@@ -70,4 +92,4 @@ class Recommender:
 
     def clear_purchases(self):
         for id in Product.objects.values_list("id", flat=True):
-            r.delete(self.get_product_key(id))
+            self._cache_storage.delete(self.get_product_key(id))
